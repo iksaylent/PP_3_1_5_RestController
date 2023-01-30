a83801aa20181f54e2b2883283de4242a8dfceb7
andrejvackov <iksaylent@yandex.ru>
2023-01-30 19:27:27 +0500

    1.9 Исправлен контроллер

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminControllers.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminControllers.java
@@ -21,12 +21,30 @@ public class AdminControllers {
     }
 
     @GetMapping("/admin")
-    public String showAllUsers(Model model, @AuthenticationPrincipal User user) {
+    public String showAllUsers(Model model,@AuthenticationPrincipal User user) {
         model.addAttribute("users", userService.getAllUsers());
-        model.addAttribute("authUser", user);
+        model.addAttribute("authUser",user);
         model.addAttribute("allRoleList", roleService.getAllRoles());
         model.addAttribute("newuser", new User());
         return "/admin/admin";
+    }
+
+    @PostMapping("/admin")
+    public String add(@ModelAttribute("user") User user) {
+        userService.createUser(user);
+        return "redirect:/admin/admin";
+    }
+
+    @PatchMapping("/admin/{id}/updateuser")
+    public String update(@PathVariable("id") long id, User user) {
+        userService.updateUser(user, id);
+        return "redirect:/admin/admin";
+    }
+
+    @DeleteMapping("/admin/{id}/deleteuser")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin/admin";
     }
 }
 
